d190dad7009cc1cd49330ae3219613d7f0313857
R23MJ <sheaharrington@outlook.com>
2020-02-18 11:40:47 -0800

    Hide players with less than an hour in hospital.

--- torn-hospital.js
+++ torn-hospital.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name Faction Hospital - Edited (Also hides offliners)
 // @namespace http://tampermonkey.net/
-// @version 1.4.3
+// @version 1.4.2
 // @description Shows only faction members that are in the hospital and online, and hides the rest.
-// @author muffenman and help from Pi77Bull - Modified by Goltred
+// @author muffenman and help from Pi77Bull - Modified by Goltred & Reborn121
 // @updateURL https://github.com/Goltred/tornscripts/blob/master/torn-hospital.js
 // @downloadURL https://github.com/Goltred/tornscripts/blob/master/torn-hospital.js
 // @match https://www.torn.com/factions.php?step=profile&ID=*
@@ -18,14 +18,23 @@ const hideWalls = true;
 const hideIdle = true;
 const hideOffline = true;
 const hideDescription = true;
+const threshold = 1; //> Members in the hospital for less than this value in hours will be hidden.
 
 if (hideDescription) $( ".faction-description" ).css("display", "none", "traveling"); //hides faction description
 
 if (hideOffline) $('.member-list #icon2').parents("li").hide(); //hides offliners
 if (hideIdle) $('.member-list #icon62').parents("li").hide(); //hides idles
+
 $('.member-list > li:not(:contains("Hospital"))').css("display", "none"); //hides every member that is not in hospital
 $('.member-list > li:contains("Hospital")').each((i, j) => { //loops through every member that is in hospital
   $(j).find(".days").text($(j).find("#icon15").attr("title").substr(-16, 8)); //displays time that is found in the hospital icon
+  
+  //> Hide members in the hospital for less than threshold.
+  var hours = Number($(j).find("#icon15").attr("title").substr(-16, 2));
+  if (hours < threshold) {
+     $(j).css("display", "none");
+  }
+  
   $(".title .days").text("Time"); //changes Days column title to "Time"
 });
 //console.log("Made by muffenman [2002712] and Pi77Bull [2082618] . If you like it, send us a message or a gift either is fine :P \"I love your script!\".");
@@ -39,5 +48,5 @@ if (hideWalls) {
         clearInterval(wallsCheck);
         el.css("display", "none");
     }
-  }, 100);
+  }, 1000);
 }
