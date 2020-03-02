708a583dc0b6d3e6a7de0bd24515b90d1e4ac8f1
Goltred <goltred@hotmail.com>
2020-03-02 14:16:16 -0500

    Updated version of tornFactionBank

--- tornFactionBank.js
+++ tornFactionBank.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn City - Faction Bank
 // @namespace    Goltred.Faction
-// @version      0.9
+// @version      0.10
 // @description  Display money on faction bank and online bankers
 // @author       Goltred
 // @updateURL    https://raw.githubusercontent.com/Goltred/tornscripts/master/tornFactionBank.js
