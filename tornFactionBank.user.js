c21afa884489ed2d01abbd9bcb2c0b71ea83d4f2
Goltred <goltred@hotmail.com>
2020-03-19 10:35:33 -0400

    Updated index so that API box can be used in mobile

--- tornFactionBank.user.js
+++ tornFactionBank.user.js
@@ -64,7 +64,7 @@ function showAPIInput() {
   const body = $('body');
 
   const inputBox = $(`
-<div class="info-msg" id="tcfb-apibox" style="position: absolute;top: 0;right: 0;background-color: lightgray; border-style: solid; border-left: 5px solid red; z-index: 20">
+<div class="info-msg" id="tcfb-apibox" style="position: absolute;top: 0;right: 0;background-color: lightgray; border-style: solid; border-left: 5px solid red; z-index: 100000">
   <p>Enter your API Key</p>
   <input type="text" id="tcfb-input-api" />
   <button id="tcfb-save-api">Save</button>
@@ -81,28 +81,41 @@ function tcfb_saveAPI() {
 }
 
 function displayFactionMoney(data, userData, bankers) {
-  // Copy the money point block
-  const moneyPointBlock = $('#user-money').closest('p[class^="point-block"]');
-  const newPointBlock = moneyPointBlock.clone();
-
-  // Get the label and value fields
-  const spans = newPointBlock.children("span");
-  const label = spans.first();
-  const moneySpan = spans.last();
-
   // Add the styling required for tooltip
-  GM_addStyle(".tcbf-tooltipbox { position: relative; display: inline-block; width: 100%; z-index: 20; }");
+  GM_addStyle(".tcbf-tooltipbox { position: relative; display: contents; width: 100%; z-index: ; }");
   GM_addStyle(".tcbf-tooltiptext { visibility: hidden; width: 100%; background-color: white; text-align: center; padding: 5px 0; border-radius: 6px; border: 1px solid black; position: absolute; z-index: 20;");
   GM_addStyle(".tcbf-tooltipbox:hover .tcbf-tooltiptext { visibility: visible; }");
 
   // Move things inside an a element for tooltipping
   const factionDiv = $('<div id="tcbf-block" class="tcbf-tooltipbox"></div>');
   factionDiv.append($(`<span class="tcbf-tooltiptext">${bankers}</span>`));
+
+  let moneyPointBlock;
+  let newPointBlock;
+  let moneySpan;
+  if (getUserDevice() == 'desktop') {
+    moneyPointBlock = $('#user-money').closest('p[class^="point-block"]');
+    newPointBlock = moneyPointBlock.clone();
+
+    // Get the label and value fields
+    const label = newPointBlock.children("span").first();
+
+    // Update the label and set the default value money
+    label.text('Faction:');
+  } else {
+    // we are in mobile
+    moneyPointBlock = $('#pointsMoney');
+    newPointBlock = moneyPointBlock.clone();
+    newPointBlock.id = 'factionMoney';
+  }
+
+  // Get the value field which is in a span
+  const spans = newPointBlock.children("span");
+  moneySpan = spans.last();
+
   newPointBlock.append(factionDiv);
   factionDiv.append(spans);
 
-  // Update the label and set the default value money
-  label.text('Faction:');
   moneySpan.css('color', '');
   moneySpan.text('$0');
 
