9ae9972a6970fb2c57bed4f84cb5a5c0e1584846
Goltred <goltred@hotmail.com>
2020-03-19 10:38:02 -0400

    Updated factionbank version. Updated default variable name

--- tornFactionBank.user.js
+++ tornFactionBank.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn City - Faction Bank
 // @namespace    Goltred.Faction
-// @version      0.14.3
+// @version      0.14.4
 // @description  Display money on faction bank and online bankers
 // @author       Goltred
 // @updateURL    https://raw.githubusercontent.com/Goltred/tornscripts/master/tornFactionBank.user.js
