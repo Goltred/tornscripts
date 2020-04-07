ac38431c52f93ac1f34b063a347cd8589f734ea0
Goltred <goltred@hotmail.com>
2020-04-07 00:18:55 -0400

    Added new script

---
+++ torn-move-ui.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Move Torn UI
+// @namespace    https://github.com/Goltred/tornscripts
+// @version      0.1
+// @description  Move the lists after the main content
+// @author       Goltred
+// @match        https://www.torn.com/*
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Modify the main container width
+    let mc = $('#mainContainer');
+    let currWidth = parseInt(mc.css('width'));
+    mc.css('width', currWidth + 170);
+
+    // Get the lists block
+    let lists = $('h2:contains("Lists")').closest('div[class^="sidebar-block"]');
+
+    // append a new div after the content wrapper
+    let right = $('<div></div>');
+    $('.content-wrapper').after(right);
+    right.append(lists);
+    right.css({
+        'float': 'left',
+        'margin-left': '20px'
+    });
+})();
