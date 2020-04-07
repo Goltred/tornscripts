9459029d5c807b4bc608bea54551fdf3a7f1c9cf
Goltred <goltred@hotmail.com>
2020-04-07 14:45:40 -0400

    Added auto yes click when reviving on hospital view, mini profiles and profile pages

--- torn-move-ui.user.js
+++ torn-move-ui.user.js
@@ -1,31 +1,47 @@
 // ==UserScript==
 // @name         Move Torn UI
 // @namespace    https://github.com/Goltred/tornscripts
-// @version      0.2
-// @description  Move the lists after the main content
+// @version      0.3
+// @description  Different tweaks to Torn UI's things
 // @author       Goltred
 // @match        https://www.torn.com/*
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
 
-(function() {
-    'use strict';
+// Modify the main container width
+let mc = $('#mainContainer');
+let currWidth = parseInt(mc.css('width'));
+mc.css('width', currWidth + 250);
 
-    // Modify the main container width
-    let mc = $('#mainContainer');
-    let currWidth = parseInt(mc.css('width'));
-    mc.css('width', currWidth + 250);
+// Get the lists block
+let lists = $('h2:contains("Lists")').closest('div[class^="sidebar-block"]');
 
-    // Get the lists block
-    let lists = $('h2:contains("Lists")').closest('div[class^="sidebar-block"]');
+// append a new div after the content wrapper
+let right = $('<div></div>');
+$('.content-wrapper').before(right);
+right.append(lists);
+right.css({
+    'float': 'left',
+    'margin-left': '20px'
+});
 
-    // append a new div after the content wrapper
-    let right = $('<div></div>');
-    $('.content-wrapper').after(right);
-    right.append(lists);
-    right.css({
-        'float': 'left',
-        'margin-left': '20px'
-    });
-})();
+function waitForTable() {
+    setTimeout(() => {
+        let fs = $('.fullScreen');
+        if (fs.length > 0) {
+            fs.css('float', 'left');
+        } else {
+            waitForTable();
+        }
+    }, 100);
+}
+
+// Handle poker modifications
+if (document.URL.includes('loader.php?sid=holdemFull')) {
+    console.log('poker');
+    $('div.content-wrapper').attr('style', 'width: 100% !important');
+    let root = $('div.content-wrapper');
+
+    waitForTable();
+}
