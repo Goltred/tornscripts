ac4f3a9723a9e5f0181688cc0b3ac1f7a98561b2
Goltred <goltred@hotmail.com>
2020-12-08 14:23:12 -0500

    Changed tornCompanyManager.user.js to not overwrite values already input in stock boxes. Added "True Stock" value to total column, depicting total stock + in transit stock. Use true stock value for purchase calculation

--- tornCompanyManager.user.js
+++ tornCompanyManager.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn City - Company Manager
 // @namespace    Goltred.Company
-// @version      0.8.2
+// @version      0.9.0
 // @description  Calculate stock reorder based on sales ratio and max storage capacity
 // @author       Goltred
 // @updateURL    https://raw.githubusercontent.com/Goltred/tornscripts/master/tornCompanyManager.user.js
@@ -15,7 +15,15 @@
 $(document).ajaxComplete((evt, xhr, settings) => {
   if (settings.url.includes('companies.php?step=stock')) {
     logger.debug('Found stocks element. We are done waiting!');
-    calculateStock();
+    // Get total stock. Number comes with a ',' which is removed to have a full int
+    let totalStock = parseInt(removeThousandsSep($("li.total > div.stock.bold").text().trim()));
+    logger.debug(`Total stock value: ${totalStock}`);
+    let inTransitStock = getStockInTransit();
+    logger.debug(`Total in transit stock: ${inTransitStock}`);
+    const trueTotal = inTransitStock + totalStock;
+
+    calculateStock(trueTotal);
+    addStockInTransitSpan(trueTotal);
   }
 });
 
@@ -30,15 +38,11 @@ const logger = new Logger('tornCompanyManager'); // pass 'debug' here to enable 
 let manageCompany = $('.manage-company');
 logger.debug('menu element found', manageCompany);
 
-function calculateStock() {
-  // Get total stock. Number comes with a ',' which is removed to have a full int
-  let totalStock = parseInt($("li.total > div.stock.bold").text().trim().replace(',', ''));
-  logger.debug(`Total stock value: ${totalStock}`);
-
-  let totalSold = parseInt($("li.total > div.sold-daily.bold").text().trim().replace(',', ''));
+function calculateStock(trueStock) {
+  let totalSold = parseInt(removeThousandsSep($("li.total > div.sold-daily.bold").text().trim()));
   logger.debug(`Total sold value: ${totalSold}`);
 
-  let availableStock = maxStorage - totalStock;
+  let availableStock = maxStorage - trueStock;
   logger.debug(`Available stock to buy: ${availableStock}`);
 
   // Start looping on rows
@@ -55,7 +59,7 @@ function calculateStock() {
     logger.debug(`Row #${idx}: Input div: ${qInput}`);
 
     // Get the current percentages
-    let soldQ = parseInt(clearText($(rowInfo).find('div.sold-daily').first()).replace(',', '').trim());
+    let soldQ = parseInt(removeThousandsSep(clearText($(rowInfo).find('div.sold-daily').first())));
     logger.debug(`Row #${idx}: Quantity sold: ${soldQ}`);
 
     let soldP = soldQ / totalSold;
@@ -65,15 +69,42 @@ function calculateStock() {
     let amount = parseInt(availableStock * soldP);
     logger.debug(`Row #${idx}: Amount to buy is ${amount}`);
 
-    qInput.val(amount);
-    qInput.blur(); // This triggers validation on the field
+    // Override only when a value is not set yet
+    console.log(qInput.val())
+    if (qInput.val() === '') {
+      qInput.val(amount);
+      qInput.blur(); // This triggers validation on the field
+    }
   });
+}
+
+function getStockInTransit() {
+  const inTransit = $('ul.order-list').find('div.status:not(:contains("Delivered"))');
+  let total = 0;
+  if (inTransit.length === 0) return total;
+
+  $.each(inTransit, (idx, el) => {
+    total += parseInt(removeThousandsSep($(el.previousElementSibling).text()));
+  });
+
+  return total;
+}
+
+function addStockInTransitSpan(spanValue) {
+  const totalStorage = $("li.total > div.stock.bold");
+  const newSpan = $('<span style="color:red; font-weight: normal;"></span>');
+  newSpan.text(`(${spanValue})`);
+  totalStorage.append(newSpan);
+}
+
+function removeThousandsSep(str) {
+  return str.replace(',', '');
 }
 
 // Torn stock lines usually contain a span element inside that we want to remove
 function clearText(el) {
   let clone = el.clone();
 
-  $(clone).find('span').remove()
-  return clone.text().trim()
+  $(clone).find('span').remove();
+  return clone.text().trim();
 }
